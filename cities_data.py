01f515f3800d431d5e70f28d302a221828a72adb
Katrina Koenders <koend001@umn.edu>
2020-04-02 22:19:07 -0500

    update

--- cities_data.py
+++ cities_data.py
@@ -1,19 +1,8 @@
 import pandas as pd
 import os
-
 # File to Load (Remember to Change These)
 file_to_load = os.path.join("WebVizualizations","Resources","cities.csv")
-
 # Read Purchasing File and store into Pandas data frame
-
-
 cities_data = pd.read_csv(file_to_load)
-
-
-
 print(cities_data)
-
-
-cities_data.to_html()
-
-cities_df = cities_data({'City_ID','City','Cloudiness','Country','Date'})
+cities_data.to_html('table.html', index=False, classes=['table','table-striped','table-hover'])
